df53d2dea663eabc89fb0e398b215a01216d1e0a
Mike Weber <mike.weber@govdelivery.com>
2019-11-10 14:45:51 -0600

    Red: Add spec we should have added first to write the held_open method

--- spec/door_spec.rb
+++ spec/door_spec.rb
@@ -46,6 +46,15 @@ describe Door do
     end.to change { door.open? }.from(true).to(false)
   end
 
+  it 'can be held open' do
+    door = Door.new
+    expect(door.held_open).to be(false)
+
+    door.held_open = true
+
+    expect(door.held_open).to be(true)
+  end
+
   it 'cannot be closed when being held open' do
     door = Door.new
     door.open!
