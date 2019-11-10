52c541f96df5a8d3aedd2deb4083312503c56b3b
Mike Weber <mike.weber@govdelivery.com>
2019-11-09 23:06:11 -0600

    Refactor: group initial specs together

--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -13,18 +13,20 @@ class Elevator
 end
 
 describe Elevator do
-  it 'starts on a floor' do
-    elevator = Elevator.new
-    expect(elevator.floor).to eq(0)
-  end
+  context 'initial state' do
+    it 'starts on a floor' do
+      elevator = Elevator.new
+      expect(elevator.floor).to eq(0)
+    end
 
-  it 'starts with a status of waiting' do
-    elevator = Elevator.new
-    expect(elevator.status).to eq('waiting')
-  end
+    it 'starts with a status of waiting' do
+      elevator = Elevator.new
+      expect(elevator.status).to eq('waiting')
+    end
 
-  it 'starts with the doors closed' do
-    elevator = Elevator.new
-    expect(elevator).to_not be_open
+    it 'starts with the doors closed' do
+      elevator = Elevator.new
+      expect(elevator).to_not be_open
+    end
   end
 end
