5dba385f2b622c76b62f2a12ceab17a95c88d096
Mike Weber <mike.weber@govdelivery.com>
2019-11-10 12:47:05 -0600

    Red: A door should be able to close again

--- spec/door_spec.rb
+++ spec/door_spec.rb
@@ -24,8 +24,18 @@ describe Door do
 
   it 'can be opened' do
     door = Door.new
+
     expect do
       door.open!
     end.to change { door.open? }.from(false).to(true)
+  end
+
+  it 'can be closed' do
+    door = Door.new
+    door.open!
+
+    expect do
+      door.close!
+    end.to change { door.open? }.from(true).to(false)
   end
 end
