276c008396871e6631f465ac6a0dda036f1476e0
Mike Weber <mike.weber@govdelivery.com>
2019-11-13 22:49:50 -0600

    Red: Add an expectation to the new spec that forces us to use the Elevator class

--- spec/bank.rb
+++ spec/bank.rb
@@ -8,5 +8,6 @@ describe Bank do
   it 'can store multiple elevators' do
     bank = Bank.new
     expect(bank.elevators.length).to eq(1)
+    expect(bank.elevators.first).to be_a(Elevator)
   end
 end
