2ece38de06c0c73318bda9ba649e5c9db9649baf
Mike Weber <mike.weber@govdelivery.com>
2019-11-13 22:53:35 -0600

    Red: A Bank should take in a list of elevators as an argument

--- spec/bank.rb
+++ spec/bank.rb
@@ -11,5 +11,11 @@ describe Bank do
     bank = Bank.new
     expect(bank.elevators.length).to eq(1)
     expect(bank.elevators.first).to be_a(Elevator)
+
+    elevators = [Elevator.new, Elevator.new, Elevator.new]
+    bank = Bank.new(elevators)
+    expect(bank.elevators.length).to eq(3)
+    expect(bank.elevators.first).to be_a(Elevator)
+    expect(bank.elevators.last).to be_a(Elevator)
   end
 end
