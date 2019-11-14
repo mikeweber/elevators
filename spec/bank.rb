6e9e3a6853b48acfb4e5bd03b860e33d7b2d1bba
Mike Weber <mike.weber@govdelivery.com>
2019-11-13 23:14:43 -0600

    Green: Always call the closest available elevator

--- spec/bank.rb
+++ spec/bank.rb
@@ -13,7 +13,7 @@ class Bank
   end
 
   def call_to_floor(floor)
-    elevators.detect { |el| el.waiting? }.call_to_floor(floor)
+    elevators.sort_by { |el| (el.floor - floor).abs }.detect { |el| el.waiting? }.call_to_floor(floor)
   end
 
   def step!
