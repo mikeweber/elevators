8c9e8c49e07e258d8bed491ab75367504f92e9d5
Mike Weber <mike.weber@govdelivery.com>
2019-11-13 23:58:03 -0600

    Green: As soon as an Elevator is available, call it with queued requests

--- spec/bank.rb
+++ spec/bank.rb
@@ -10,16 +10,21 @@ class Bank
 
   def initialize(elevators = [Elevator.new])
     self.elevators = elevators
+    @queue = []
   end
 
   def call_to_floor(floor)
-    return unless elevator = sorted_eligible_elevators(floor).first
+    return add_to_queue!(floor) unless elevator = sorted_eligible_elevators(floor).first
 
     elevator.call_to_floor(floor)
   end
 
   def step!
+    old_queue = @queue
+    @queue = []
+
     elevators.each { |el| el.step! }
+    old_queue.each { |floor| call_to_floor(floor) }
   end
 
   def floors
@@ -38,6 +43,10 @@ class Bank
 
   def sorted_eligible_elevators(requested_floor)
     elevators.select { |el| el.waiting? || el.going_down? && requested_floor < el.floor || el.going_up? && requested_floor > el.floor }.sort_by { |el| (requested_floor - el.floor).abs }
+  end
+
+  def add_to_queue!(floor)
+    @queue << floor
   end
 end
 
